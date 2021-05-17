6aa62fa6f8499f552c4fb35b215abf30a4e50810
Sayali Kukkar <sayukukkar11@gmail.com>
2021-05-17 13:23:28 +0530

    Updates

---
+++ precache-manifest.65c5dc3d1ad2a7fbd641cb4ee44801b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677ae41ec0abf5adb1f9e647e1cf948f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7d738286967c9311b2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f713a361899da2fded3b",
+    "url": "/static/js/2.31f1f365.chunk.js"
+  },
+  {
+    "revision": "7c7d738286967c9311b2",
+    "url": "/static/js/main.93e6e2d3.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
